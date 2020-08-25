a2f96f53bfe3a7f22687a2d795ca5d153b28ffe9
Radoslaw Hulboj <radoslaw.hulboj@capgemini.com>
2020-08-25 13:10:48 +0200

    clean code for Builder

--- src/main/java/dp/builder/MainBuilder.java
+++ src/main/java/dp/builder/MainBuilder.java
@@ -5,6 +5,22 @@ import eu.hulboj.model.Person;
 
 public class MainBuilder {
     public static void main(String[] args) {
+        buildBob();
+        buildAnna();
+    }
+
+    public static void buildAnna() {
+        Person anna = IBuilder.of(Person::new)
+                .with(Person::setAll, "Anna", 24)
+                .with(Person::setHeart,
+                        IBuilder.of(Heart::new)
+                                .with(Heart::setHealth, "super good")
+                                .build())
+                .build();
+        System.out.println(anna);
+    }
+
+    public static void buildBob() {
         Person rob = IBuilder.of(Person::new)
                 .with(Person::setAge, 23)
                 .with(Person::setName, "Rob")
@@ -14,14 +30,5 @@ public class MainBuilder {
                                 .build())
                 .build();
         System.out.println(rob);
-
-        Person anna = IBuilder.of(Person::new)
-                .with(Person::setAll, "Anna", 24)
-                .with(Person::setHeart,
-                        IBuilder.of(Heart::new)
-                                .with(Heart::setHealth, "super good")
-                                .build())
-                .build();
-        System.out.println(anna);
     }
 }
