a278411bbc3b63aef78e2ace09a1c754bc81f210
Hulboj <radoslaw.hulboj@capgemini.com>
2020-11-13 16:31:23 +0100

    [clean code] - removing german umlauts

--- src/main/java/cookbook/cache/CacheUtils.java
+++ src/main/java/cookbook/cache/CacheUtils.java
@@ -3,36 +3,19 @@ package cookbook.cache;
 import java.util.Objects;
 import java.util.function.Supplier;
 
-/**
- * Wenn einige Methoden mehrmals aufgerufen sind, kann diese Klasse verwendet werden,
- * um die Methode nur einmal aufzurufen. Nächste Aufrufe der Methode werden vermieden und
- * der Rückgabewert wird aus dem Cache ermittelt.
- *
- * @param <V> Der generische Parameter, der zwichengespeichert werden soll.
- */
+
 public final class CacheUtils<V> implements Supplier<V> {
-    /**
-     * Behält das Ergebnis des V-Paramerers bei.
-     */
+
     private Supplier<? extends V> mSupplier;
-    /**
-     * gecached Wert
-     */
+
     private V mValue;
 
-    /**
-     * Die Klasse arbeitet als Utility Klasse ( kein öffentlicher Konstrukteur )
-     *
-     * @param supplier Supplier
-     */
+
     private CacheUtils(Supplier<? extends V> supplier) {
         this.mSupplier = supplier;
     }
 
-    /**
-     * @param supplier Supplier, die zwischengespeichert werden soll
-     * @return Supplier mit Rückgabewert
-     */
+
     public static <V> Supplier<V> memoize(Supplier<V> supplier) {
         return supplier instanceof CacheUtils ? (CacheUtils) supplier : new CacheUtils<>(supplier);
     }
@@ -42,9 +25,7 @@ public final class CacheUtils<V> implements Supplier<V> {
         return Objects.isNull(mSupplier) ? this.mValue : this.computeValueIfSupplier();
     }
 
-    /**
-     * @return Wert aus dem Supplier
-     */
+
     private synchronized V computeValueIfSupplier() {
         Supplier<? extends V> supp = this.mSupplier;
         if (Objects.nonNull(supp)) {
