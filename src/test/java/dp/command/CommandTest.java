89334f721c1cb474fb4197ffe47f51ec3c250fd4
Hulboj <radoslaw.hulboj@capgemini.com>
2020-11-17 21:05:36 +0100

    [master] - removing spock test and adding command dp test

---
+++ src/test/java/dp/command/CommandTest.java
@@ -0,0 +1,31 @@
+package dp.command;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static dp.command.ExecuteCommand.CommandType.Executor;
+import static dp.command.ExecuteCommand.CommandType.Victim;
+
+
+class CommandTest {
+
+    @Test
+    @DisplayName("The command object run 3 times mocked method execute")
+    void commandExecutedThreeTimes() {
+        // given
+        ExecuteCommand mock = Mockito.mock(ExecuteCommand.class);
+        Command<ExecuteCommand.CommandType> of = Command.of(Command
+                .<ExecuteCommand.CommandType>createRegister()
+                .register(Executor, mock::execute)
+                .register(Victim, mock::execute));
+        // when
+        of.execute(Executor);
+        of.execute(Executor);
+        of.execute(Victim);
+        // then
+        Mockito.verify(mock, Mockito.times(2)).execute(Executor);
+        Mockito.verify(mock, Mockito.times(1)).execute(Victim);
+
+    }
+}
---
+++ src/test/java/dp/command/ExecuteCommand.java
@@ -0,0 +1,11 @@
+package dp.command;
+
+class ExecuteCommand {
+    enum CommandType {
+        Executor,
+        Victim
+    }
+    void execute(CommandType cmdType) {
+
+    }
+}
