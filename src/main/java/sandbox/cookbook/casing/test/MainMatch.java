3713ac6a05146c92bf9a4e61f5333a56f2dac4c5
Radoslaw_Hulboj <radekhulboj@gmail.com>
2021-06-20 13:27:51 +0200

    MainMatch, function recognise the type of the class (known from Visit pattern

--- src/main/java/sandbox/cookbook/casing/test/MainMatch.java
+++ src/main/java/sandbox/cookbook/casing/test/MainMatch.java
@@ -7,23 +7,39 @@ import static sandbox.cookbook.casing.test.Switcher.Match;
 
 public class MainMatch {
 
-    static class A { }
-    static class B extends A{}
-    static class C extends A{}
+    static class A {
+    }
 
+    static class B extends A {
+    }
 
     public static void main(String[] args) {
+        test1();
+        test2();
+    }
+
+    private static void test2() {
+        A AA = new A();
+        B BB = new B();
+        A searchValue = BB;
+        Optional<Object> of = Match(searchValue).of(
+                $(AA, MainMatch::b),
+                $(BB, () -> "BB")
+        );
+        System.out.println(of.get());
+    }
+
+    private static void test1() {
         String DWA = "2";
         String searchValue = "1";
         Optional<Object> of = Match(searchValue).of(
                 $(DWA, MainMatch::b),
                 $("1", () -> 2)
-                );
+        );
         System.out.println(of.get());
-
     }
 
-    static String b () {
-        return "";
+    static String b() {
+        return "-";
     }
 }
