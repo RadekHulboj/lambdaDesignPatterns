475b0ba81ce06e773a168446228bd28447f56335
Radoslaw Hulboj <radekhulboj@gmail.com>
2024-02-23 19:38:55 +0100

    builder with no setters for built class

--- src/main/java/dp/builder/nosetters/MainCatBuilder.java
+++ src/main/java/dp/builder/nosetters/MainCatBuilder.java
@@ -1,7 +1,6 @@
 package dp.builder.nosetters;
 
 import dp.builder.nosetters.ICatBuilder.CatBuilder;
-
 public class MainCatBuilder {
     public static void main (String... args) {
         Cat cat = ICatBuilder.of()
